c599ce84c81db068f8350ca26657fee2981ccd05
RA1010 <r.aoki.7.10.10@gmail.com>
2019-05-22 01:30:58 +0900

    add:エンティティー追加

---
+++ mrs/src/main/java/mrs/domain/model/Reservation.java
@@ -0,0 +1,41 @@
+package mrs.domain.model;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Reservation implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer resevationId;
+
+	private LocalTime startTime;
+
+	private LocalTime endTime;
+
+	@ManyToOne
+	@JoinColumns({ @JoinColumn(name = "reserved_date"),
+		@JoinColumn(name = "room_Id") })
+	private ReservableRoom reservableRoom;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+}
+
+
+
+
+
+
+
+
+
